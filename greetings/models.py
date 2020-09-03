aae81265e58aafe5b3cb924c4495f983466e3a63
kenyanroot <ndedanaco@gmail.com>
2020-09-03 22:57:18 +0300

    trying to fix issue of data from saf not showing up

--- greetings/models.py
+++ greetings/models.py
@@ -6,6 +6,6 @@ class Namecomment(models.Model):
     phone_number=models.CharField(max_length=12 )
     name=models.TextField(blank= True)
     def __str__(self):
-        return f"{self.name} has said{self.comment} .Theit number is >>{self.phone_number}"
+        return f"{self.name} has said  {self.comment} .Theit number is >>{self.phone_number}"
 
 
--- serverresponse/models.py
+++ serverresponse/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class  LMNOnline(models.Model):
-    Merchant_request_ID=models.CharField(max_length=50)
-    Checkout_request_ID=models.CharField(max_length=20)
-    Result_code=models.IntegerField()
-    Result_desc =models.CharField(max_length=120)
-    Ammount     =models.FloatField()
-    Mpesa_receipt_number=models.CharField(max_length=20)
-    Transaction_date =models.DateTimeField()
-    Phone_number =models.CharField(max_length=13)
+    Merchant_requestID=models.CharField(max_length=50)
+    Checkout_requestID=models.CharField(max_length=20)
+    Resultcode=models.IntegerField()
+    Resultdesc =models.CharField(max_length=120)
+    A_mmount     =models.FloatField()
+    Mpesa_receiptnumber=models.CharField(max_length=20)
+    Transactiondate =models.DateTimeField()
+    Phonenumber =models.CharField(max_length=13)
     def __str__(self):
-        return f"{self.Phone_number} has sent {self.Ammount} at {self.Transaction_date}"
+        return f"{self.Phonenumber} has sent  {self.A_mmount} at {self.Transactiondate}"
