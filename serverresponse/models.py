3184d80b1413c1bd13fe269d5ef0730121950493
kenyanroot <ndedanaco@gmail.com>
2020-09-04 11:46:45 +0300

    changed len to 40

--- serverresponse/models.py
+++ serverresponse/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class  LMNOnline(models.Model):
     Merchant_requestID=models.CharField(max_length=50)
-    Checkout_requestID=models.CharField(max_length=50)
+    Checkout_requestID=models.CharField(max_length=40)
     Resultcode=models.IntegerField()
     Resultdesc =models.CharField(max_length=120)
     A_mmount     =models.FloatField()
