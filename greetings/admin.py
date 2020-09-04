551fd28205c3f03bd2e0cac18eab7c6d390e5243
kenyanroot <ndedanaco@gmail.com>
2020-09-04 14:51:03 +0300

    brrrrrrr

--- greetings/admin.py
+++ greetings/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from  greetings.models import Namecomment
-admin.site.register(Namecomment)
 
 
 class NamecommentAdmin(admin.ModelAdmin):
     list_display = ('name','comment','phone_number')
+
+admin.site.register(Namecomment,NamecommentAdmin)
--- serverresponse/admin.py
+++ serverresponse/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from  serverresponse.models import LMNOnline
 
-admin.site.register(LMNOnline)
+
 
 class LMNOnlineAdmin(admin.ModelAdmin):
     list_display = ('Phonenumber','A_mmount','Transactiondate','Resultdesc')
+
+admin.site.register(LMNOnline,LMNOnlineAdmin)
